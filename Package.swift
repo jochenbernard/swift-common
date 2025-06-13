3bb8b17b135b48b12bc503e3bf8c99fdb8e56875
Jochen Bernard <jochenbernard@gmail.com>
2025-06-14 01:04:00 +0200

    Add focus on appear

--- Package.swift
+++ Package.swift
@@ -4,14 +4,27 @@ import PackageDescription
 
 let package = Package(
     name: "SwiftCommon",
+    platforms: [
+        .iOS(.v15),
+        .macCatalyst(.v15),
+        .macOS(.v12),
+        .tvOS(.v15),
+        .visionOS(.v1),
+        .watchOS(.v8),
+    ],
     products: [
         .library(
             name: "FoundationCommon",
             targets: ["FoundationCommon"]
+        ),
+        .library(
+            name: "SwiftUICommon",
+            targets: ["SwiftUICommon"]
         )
     ],
     targets: [
         .target(name: "FoundationCommon"),
+        .target(name: "SwiftUICommon"),
         .testTarget(
             name: "FoundationCommonTests",
             dependencies: ["FoundationCommon"]
---
+++ Sources/SwiftUICommon/FocusOnAppear.swift
@@ -0,0 +1,51 @@
+import SwiftUI
+
+private struct FocusOnAppear: ViewModifier {
+    private let focusOnAppear: Bool
+
+    @FocusState private var isFocused
+
+    init(_ focusOnAppear: Bool) {
+        self.focusOnAppear = focusOnAppear
+    }
+
+    func body(content: Content) -> some View {
+        content
+            .focused($isFocused)
+            .onAppear {
+                if focusOnAppear {
+                    isFocused = true
+                }
+            }
+    }
+}
+
+public extension View {
+    func focusOnAppear(_ focusOnAppear: Bool = true) -> some View {
+        modifier(FocusOnAppear(focusOnAppear))
+    }
+}
+
+#Preview("FocusOnAppear") {
+    TextField(
+        "Focus on appear",
+        text: .constant("")
+    )
+    .focusOnAppear()
+}
+
+#Preview("FocusOnAppearTrue") {
+    TextField(
+        "Focus on appear true",
+        text: .constant("")
+    )
+    .focusOnAppear(true)
+}
+
+#Preview("FocusOnAppearFalse") {
+    TextField(
+        "Focus on appear false",
+        text: .constant("")
+    )
+    .focusOnAppear(false)
+}
