bd01b08921a31abbbabdcb131c853809fbbd0a32
Jochen Bernard <jochenbernard@gmail.com>
2025-08-02 13:40:28 +0200

    Add more RangeReplaceableCollection extensions

--- Sources/SwiftCommon/Extensions/RangeReplaceableCollection.swift
+++ Sources/SwiftCommon/Extensions/RangeReplaceableCollection.swift
@@ -9,6 +9,16 @@ public extension RangeReplaceableCollection where Element: Identifiable {
         removeAll(element.id)
     }
 
+    mutating func removeAll(_ ids: Set<Element.ID>) {
+        removeAll {
+            ids.contains($0.id)
+        }
+    }
+
+    mutating func removeAll(_ elements: [Element]) {
+        removeAll(Set(elements.map(\.id)))
+    }
+
     func removingAll(_ id: Element.ID) -> Self {
         filter {
             $0.id != id
@@ -17,5 +27,15 @@ public extension RangeReplaceableCollection where Element: Identifiable {
 
     func removingAll(_ element: Element) -> Self {
         removingAll(element.id)
+    }
+
+    func removingAll(_ ids: Set<Element.ID>) -> Self {
+        filter {
+            !ids.contains($0.id)
+        }
+    }
+
+    func removingAll(_ elements: [Element]) -> Self {
+        removingAll(Set(elements.map(\.id)))
     }
 }
