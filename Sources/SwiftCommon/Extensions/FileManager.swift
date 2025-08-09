2a7598bbc89339ee7a2d78cbeeed019bc7a4e9e2
Jochen Bernard <jochenbernard@gmail.com>
2025-08-09 16:50:29 +0200

    Add file manager and url documentation

--- Sources/SwiftCommon/Extensions/FileManager.swift
+++ Sources/SwiftCommon/Extensions/FileManager.swift
@@ -8,10 +8,33 @@ import Foundation
     *
 )
 public extension FileManager {
+    /// Returns a boolean value that indicates whether a file or directory exists at the specified URL.
+    ///
+    /// - Parameter url: The URL of the file or directory. App Sandbox does not restrict which URL values may be passed
+    ///                  to this parameter.
+    /// - Returns: `true` if a file at the given URL exists, or `false` if the file does not exist or its existence
+    ///            could not be determined.
+    ///
+    /// If the file at `url` is inaccessible to your app, perhaps because one or more parent directories are
+    /// inaccessible, this method returns `false`. If the final element in `url` specifies a symbolic link, this method
+    /// traverses the link and returns `true` or `false` based on the existence of the file at the link destination.
     func fileExists(at url: URL) -> Bool {
         fileExists(atPath: url.path(percentEncoded: false))
     }
 
+    /// Returns a boolean value that indicates whether a file or directory exists at the specified URL.
+    ///
+    /// - Parameter url: The URL of the file or directory.
+    /// - Parameter isDirectory: Upon return, contains `true` if `url` is a directory or if the final path element is a
+    ///                          symbolic link that points to a directory; otherwise, contains `false`. If `url` doesn't
+    ///                          exist, this value is undefined upon return. Pass `nil` if you do not need this
+    ///                          information.
+    /// - Returns: `true` if a file at the given URL exists, or `false` if the file does not exist or its existence
+    ///            could not be determined.
+    ///
+    /// If the file at `url` is inaccessible to your app, perhaps because one or more parent directories are
+    /// inaccessible, this method returns `false`. If the final element in `url` specifies a symbolic link, this method
+    /// traverses the link and returns `true` or `false` based on the existence of the file at the link destination.
     func fileExists(
         at url: URL,
         isDirectory: UnsafeMutablePointer<ObjCBool>?
--- Sources/SwiftCommon/Extensions/URL.swift
+++ Sources/SwiftCommon/Extensions/URL.swift
@@ -1,6 +1,8 @@
 import Foundation
 
 public extension URL {
+    /// The last path component of the URL after removing any path extension, or an empty string if the path is an empty
+    /// string.
     var filename: String {
         deletingPathExtension().lastPathComponent
     }
