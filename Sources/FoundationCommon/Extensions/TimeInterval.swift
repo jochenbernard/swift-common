ca73fba3411ddff5de2be1d82fd879f9c3036d57
Jochen Bernard <jochenbernard@gmail.com>
2025-08-09 16:29:58 +0200

    Add collection and sequence documentation

--- Sources/FoundationCommon/Extensions/TimeInterval.swift
+++ Sources/FoundationCommon/Extensions/TimeInterval.swift
@@ -32,40 +32,40 @@ public extension TimeInterval {
 
     /// Returns a `TimeInterval` equal to the specified number of seconds.
     ///
-    /// - parameter seconds: The number of seconds.
-    /// - returns: A `TimeInterval` equal to the provided number of seconds.
+    /// - Parameter seconds: The number of seconds.
+    /// - Returns: A `TimeInterval` equal to the provided number of seconds.
     static func seconds(_ seconds: Double) -> TimeInterval {
         seconds * oneSecond
     }
 
     /// Returns a `TimeInterval` equal to the specified number of minutes.
     ///
-    /// - parameter minutes: The number of minutes.
-    /// - returns: A `TimeInterval` equal to the provided number of minutes.
+    /// - Parameter minutes: The number of minutes.
+    /// - Returns: A `TimeInterval` equal to the provided number of minutes.
     static func minutes(_ minutes: Double) -> TimeInterval {
         minutes * oneMinute
     }
 
     /// Returns a `TimeInterval` equal to the specified number of hours.
     ///
-    /// - parameter hours: The number of hours.
-    /// - returns: A `TimeInterval` equal to the provided number of hours.
+    /// - Parameter hours: The number of hours.
+    /// - Returns: A `TimeInterval` equal to the provided number of hours.
     static func hours(_ hours: Double) -> TimeInterval {
         hours * oneHour
     }
 
     /// Returns a `TimeInterval` equal to the specified number of days.
     ///
-    /// - parameter days: The number of days.
-    /// - returns: A `TimeInterval` equal to the provided number of days.
+    /// - Parameter days: The number of days.
+    /// - Returns: A `TimeInterval` equal to the provided number of days.
     static func days(_ days: Double) -> TimeInterval {
         days * oneDay
     }
 
     /// Returns a `TimeInterval` equal to the specified number of weeks.
     ///
-    /// - parameter weeks: The number of weeks.
-    /// - returns: A `TimeInterval` equal to the provided number of weeks.
+    /// - Parameter weeks: The number of weeks.
+    /// - Returns: A `TimeInterval` equal to the provided number of weeks.
     static func weeks(_ weeks: Double) -> TimeInterval {
         weeks * oneWeek
     }
--- Sources/SwiftCommon/Extensions/Collection.swift
+++ Sources/SwiftCommon/Extensions/Collection.swift
@@ -1,8 +1,22 @@
 public extension Collection where Element: Identifiable {
+    /// Returns the first index in which an element of the collection has the specified id.
+    ///
+    /// - Parameter id: The id.
+    /// - Returns: The index of the first element which has the given id. If no elements in the collection have the
+    ///            given id, returns `nil`.
+    ///
+    /// - Complexity: O(*n*), where *n* is the length of the collection.
     func firstIndex(for id: Element.ID) -> Index? {
         firstIndex(where: { $0.id == id })
     }
 
+    /// Returns the first index in which an element of the collection has the same id as the specified element.
+    ///
+    /// - Parameter element: The element.
+    /// - Returns: The index of the first element which has the same id as the given element. If no elements in the
+    ///            collection have the same id as the given element, returns `nil`.
+    ///
+    /// - Complexity: O(*n*), where *n* is the length of the collection.
     func firstIndex(for element: Element) -> Index? {
         firstIndex(for: element.id)
     }
