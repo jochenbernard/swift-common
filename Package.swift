fbef9eeb1a946cd238d09945b50f4c7a70ecfd90
Jochen Bernard <jochenbernard@gmail.com>
2025-08-10 18:14:13 +0200

    Add pages workflow

--- Package.swift
+++ Package.swift
@@ -30,6 +30,10 @@ let package = Package(
         .package(
             url: "https://github.com/SimplyDanny/SwiftLintPlugins",
             from: "0.0.0"
+        ),
+        .package(
+            url: "https://github.com/apple/swift-docc-plugin",
+            from: "1.0.0"
         )
     ],
     targets: [
