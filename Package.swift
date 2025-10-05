5953a58a4cdad2c56a56640c687d2d56e85bfc18
Jochen Bernard <jochenbernard@gmail.com>
2025-10-05 15:29:12 +0200

    Remove unused dependency

--- Package.swift
+++ Package.swift
@@ -30,10 +30,6 @@ let package = Package(
         .package(
             url: "https://github.com/SimplyDanny/SwiftLintPlugins",
             from: "0.1.0"
-        ),
-        .package(
-            url: "https://github.com/apple/swift-docc-plugin",
-            from: "1.0.0"
         )
     ],
     targets: [
